8220a3051d367f396bcfd3fd6de07963ea9dad42
Ted Dino <ted23@tutanota.com>
2021-11-21 00:08:56 +0800

    arrange  folder structure

---
+++ front-end/src/Components/DemoVideo.js
@@ -0,0 +1,7 @@
+export default function DemoVideo() {
+  return (
+    <div>
+      <h1>Hey</h1>
+    </div>
+  );
+}
--- front-end/src/Components/HeroSection.js
+++ front-end/src/Components/HeroSection.js
@@ -1,15 +1,14 @@
-import "../Styles/hero_section.css";
+import "../Styles/HeroSection/hero_section.css";
 
 export default function HeroSection() {
   return (
-    <div className="container main">
+    <div className="container row main">
       <div className="main__description">
         <h1 className="main__title">Lorem Ipsum</h1>
         <p className="main__subtitle">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
           omnis voluptate animi dolorem deserunt. Aut similique pariatur culpa
-          vitae modi repellat, illum ratione expedita, laudantium laboriosam
-          quisquam praesentium officia neque!
+          vitae modi repellat.
         </p>
         <button className="main__button">Try it!</button>
       </div>
