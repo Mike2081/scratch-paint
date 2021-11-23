9123e0574eddda7a88160866fe553d9399b38637
Michael DoHo <michaeldoho@hotmail.com>
2021-11-23 11:23:54 -0500

    experimenting with canvasFunctions

--- front-end/src/Components/CanvasFunctions.js
+++ front-end/src/Components/CanvasFunctions.js
@@ -1,15 +1,42 @@
 import React, { useContext, useRef, useState } from "react";
+import io from 'socket.io-client';
 
 
 
     const CanvasFunctions = React.createContext();
 
+    
     export const CanvasProvider = ({ children }) => {
         const [beginDrawing, setbeginDrawing] = useState(false)
         const canvasRef = useRef(null);
         const contextRef = useRef(null);
         //^ will be wrapped around <App/> in index.js, 
         // so these hooks can be used on different components
+
+        //START
+        
+        const socket = io.connect("http://localhost:5000");    
+    
+        socket.on("canvas-data", function(data){
+    
+                const root = this;
+                const interval = setInterval(function(){
+                    if(root.beginDrawing) return;
+                    root.beginDrawing = true;
+                    clearInterval(interval);
+                    const image = new Image();
+                    const canvas = setupCanvas();
+                    const ctx = setupCanvas.getContext('2d');
+                    image.onload = function() {
+                        ctx.drawImage(image, 0, 0);
+    
+                        root.beginDrawing = false;
+                    };
+                    image.src = data;
+                }, 200)
+            })
+
+            //END
 
         const setupCanvas = () => {
             const canvas = canvasRef.current
@@ -44,6 +71,7 @@ import React, { useContext, useRef, useState } from "react";
             //when mouse is let go, set set beginDrawing to false
         };
 
+        const root = this;
         const draw = ({ nativeEvent }) => {
             if (!beginDrawing) { //if beginDrawing is set to true than return
             return;
@@ -51,6 +79,13 @@ import React, { useContext, useRef, useState } from "react";
             const { offsetX, offsetY } = nativeEvent; //nativeEvent grabs the mouse movements
             contextRef.current.lineTo(offsetX, offsetY); //draws a line from new to old cordinates
             contextRef.current.stroke(); //this is what draws the that line
+            //this is suppose to share the whiteboard **START**
+            if(root != undefined) clearTimeout(root);
+            setTimeout(function(){
+                const base64ImageData = toDataURL("image/png");
+                root.socket.emit("canvas-data", base64ImageData);
+            }, 1000)
+            //**END */
         };
 
         const eraseCanvas = () => {
